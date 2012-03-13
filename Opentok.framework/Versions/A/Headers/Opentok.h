246c4c29983e22bba94988e78d357d9b79337ba4
Ankur Oberoi <aoberoi@gmail.com>
2012-03-13 15:42:05 -0400

    adding opentok static framework and build script for removing duplicate symbols

---
+++ Opentok.framework/Versions/A/Headers/Opentok.h
@@ -0,0 +1,16 @@
+//
+//  opentok_ios_sdk.h
+//  opentok-ios-sdk
+//
+//  Created by Charley Robinson on 10/21/11.
+//  Copyright (c) 2011 Tokbox, Inc. All rights reserved.
+//
+
+@class OTPublisher, OTSession, OTSubscriber, OTConnection, OTStream;
+
+#import "OTPublisher.h"
+#import "OTSession.h"
+#import "OTSubscriber.h"
+#import "OTStream.h"
+#import "OTConnection.h"
+
